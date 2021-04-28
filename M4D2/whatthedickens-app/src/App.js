cfe7654844087115a2b0187a8a955696859c9c0d
Helena Nel <mailhedri@gmail.com>
2021-04-28 14:08:18 +0200

    commit

--- M4D2/whatthedickens-app/src/App.js
+++ M4D2/whatthedickens-app/src/App.js
@@ -4,19 +4,18 @@ import "bootstrap/dist/css/bootstrap.min.css";
 //Componets
 import BookList from "./components/BookList.jsx";
 import MyNavbar from "./components/Nav.jsx";
-import Desc from "./components/Desc.jsx";
 import MyFooter from "./components/Footer";
 import MyJumbotron from "./components/Jumbotron.jsx";
-import SearchBook from "./components/SearchBook.jsx";
+// import FilterBookList from "./components/FilterbookList.jsx";
 
 const App = () => {
   return (
     <div className="App">
       <MyNavbar title="What The Dickens" />
       <MyJumbotron />
-      <SearchBook />
+      {/* <SearchBook /> */}
       <BookList />
-      <Desc />
+
       <MyFooter />
     </div>
   );
--- M4D2/whatthedickens-app/src/components/Desc.jsx
+++
@@ -1,3 +0,0 @@
-const Desc = (props) => <h1>I'M THE COMMENTS COMPONENT</h1>;
-
-export default Desc;
