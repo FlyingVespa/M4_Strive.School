1975d8353078e9e3dbaee7491a65f6d1848d624e
Helena Nel <mailhedri@gmail.com>
2021-04-30 00:48:25 +0200

    css

--- M4D2/whatthedickens-app/src/components/singleBookComponent/SingleBook.jsx
+++ M4D2/whatthedickens-app/src/components/singleBookComponent/SingleBook.jsx
@@ -1,35 +1,32 @@
 import React from "react";
-import { Row, Card, Col } from "react-bootstrap";
+import { Card, Col, Button, Accordion } from "react-bootstrap";
 import "./singleBook.css";
 
-// class SingleBook extends React.Component {
-//   selectBook = (e) => {
-//     const element = e.currentTarget;
-//     element.classList.toggle("selectedCard");
-//   };
-
-function SingleBook(book) {
-  return (
-    <Row>
-      <Col>
-        <Card
-          key={book.asin}
-          onClick={() => this.setState({ selectedBook: book })}
-          className="h-100 w-100"
-          style={{ minWidth: "250px" }}
-        >
-          <img className="d-block w-100" src={book.img} alt="BookImage" />
-          <Card.Body>
-            <h3>{book.title}</h3>
-            <p>{book.asin}</p>
-            <h4>{book.price}</h4>
-            <h4>{book.catagory}</h4>
-          </Card.Body>
-        </Card>
-      </Col>
-      ;
-    </Row>
-  );
+function selectBook(e) {
+  const element = e.currentTarget;
+  element.classList.toggle("selectedCard");
 }
 
+function SingleBook(book) {
+  // const [open, setOpen] = useState(false);
+
+  return (
+    <Col lg={3} md={4} sm={6}>
+      <Card
+        key={book.asin}
+        onClick={selectBook}
+        className="h-100 w-100 text-center"
+      >
+        <Card.Img variant="top" src={book.img} />
+        <Card.Body>
+          <h3>{book.title}</h3>
+          <p>{book.asin}</p>
+          <h4>{book.price}</h4>
+          <h4>{book.catagory}</h4>
+        </Card.Body>
+      </Card>
+    </Col>
+  );
+}
+export { selectBook };
 export default SingleBook;
