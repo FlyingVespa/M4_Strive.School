7d321d729a64feb6a7335c4e718015b9169b040f
Helena Nel <mailhedri@gmail.com>
2021-05-04 15:37:31 +0200

    ex 1 - 3 done

--- M4D7/src/components/DishComments.jsx
+++ M4D7/src/components/DishComments.jsx
@@ -1,17 +1,19 @@
-import { ListGroup } from 'react-bootstrap'
+import { ListGroup } from "react-bootstrap";
 
-const DishComments = (props) => (
-    // props.dish is the whole object
+function DishComments(props) {
+  // props.dish is the whole object
+  return (
     <div className={`mt-${props.marginTop}`}>
-        <h2>Comments for {props.dish.name}</h2>
-        <ListGroup>
-            {
-                props.dish.comments.map(c => (
-                    <ListGroup.Item key={c.id}>"{c.comment}" from {c.author}</ListGroup.Item>
-                ))
-            }
-        </ListGroup>
+      <h2>Comments for {props.dish.name}</h2>
+      <ListGroup>
+        {props.dish.comments.map((c) => (
+          <ListGroup.Item key={c.id}>
+            "{c.comment}" from {c.author}
+          </ListGroup.Item>
+        ))}
+      </ListGroup>
     </div>
-)
+  );
+}
 
-export default DishComments
+export default DishComments;
--- M4D7/src/components/MyNavBar.jsx
+++ M4D7/src/components/MyNavBar.jsx
@@ -2,21 +2,30 @@
 // always name your components starting with a capital letter (PascalCase)
 
 // a functional component is just an arrow function returning JSX
+import { Component } from "react";
+import { Navbar, Nav } from "react-bootstrap";
 
-import { Navbar, Nav } from 'react-bootstrap'
-
-const MyNavBar = (props) => (
-    <Navbar bg="dark" variant="dark" expand="lg">
-        <Navbar.Brand href="#home">{props.title} - Strive For Food</Navbar.Brand>
+class MyNavBar extends Component {
+  // const MyNavBar = (props) => (
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <Navbar bg="dark" variant="dark" expand="lg">
+        <Navbar.Brand href="#home">
+          {this.props.title} - Strive For Food
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ml-auto">
-                <Nav.Link>Menu</Nav.Link>
-                <Nav.Link>Reservations</Nav.Link>
-                <Nav.Link>Contact us</Nav.Link>
-            </Nav>
+          <Nav className="ml-auto">
+            <Nav.Link>Menu</Nav.Link>
+            <Nav.Link>Reservations</Nav.Link>
+            <Nav.Link>Contact us</Nav.Link>
+          </Nav>
         </Navbar.Collapse>
-    </Navbar>
-)
-
-export default MyNavBar
+      </Navbar>
+    );
+  }
+}
+export default MyNavBar;
