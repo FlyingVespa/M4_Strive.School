6cf39e571780e65d42c30ffb4e00d304c6be05d5
Helena Nel <mailhedri@gmail.com>
2021-05-05 16:21:54 +0200

    1
    `

--- M4D7/src/components/DishComments.jsx
+++ M4D7/src/components/DishComments.jsx
@@ -1,19 +1,33 @@
+import React, { useState } from "react";
 import { ListGroup } from "react-bootstrap";
 
-function DishComments(props) {
-  // props.dish is the whole object
+const DishComments = (props) => {
+  props.dish;
+  const [comments, setDishCommentDisplay] = useState(props.c.comment);
+
+  const dishCommentDisplayHandler = () => {
+    setDishCommentDisplay(" ");
+    console.log("dishCommentDisplay");
+  };
+
   return (
     <div className={`mt-${props.marginTop}`}>
       <h2>Comments for {props.dish.name}</h2>
       <ListGroup>
+        {props.dish.name === "Amatriciana"
+          ? this.setDishCommentDisplay("false")
+          : this.setDishCommentDisplay("true")}{" "}
+        &&
         {props.dish.comments.map((c) => (
           <ListGroup.Item key={c.id}>
-            "{c.comment}" from {c.author}
+            {c.comment.length < 1
+              ? c.comment + "No reliable reviews available"
+              : c.comment + " from " + c.author}
           </ListGroup.Item>
         ))}
       </ListGroup>
     </div>
   );
-}
+};
 
 export default DishComments;
