599e0cabe2cfd69af8127944fbc7b8018f7e3f29
Helena Nel <mailhedri@gmail.com>
2021-05-05 16:32:01 +0200

    commit

--- M4D8/netflix-react/src/App.js
+++ M4D8/netflix-react/src/App.js
@@ -1,16 +1,28 @@
 import "./App.css";
-import MyCarousel from "./components/carousel/Carousel";
 import "bootstrap/dist/css/bootstrap.min.css";
 import LoadMovies from "./components/Search/LoadMovies";
 import MyNav from "./components/Navbar/MyNav.jsx";
 import MyFooter from "./components/Footer/MyFooter";
+import ShowDetails from "./components/showDetails/ShowDetails";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <MyNav />
-      <LoadMovies genre="comedy" />
-      <LoadMovies genre="romance" />
+      <Route
+        render={(routerProps) => <LoadMovies genre="comedy" {...routerProps} />}
+        path="/"
+        exact
+      />
+      <Route
+        render={(routerProps) => (
+          <LoadMovies genre="romance" {...routerProps} />
+        )}
+        path="/"
+        exact
+      />
+      <Route component={ShowDetails} path="/details/:check" />
       <MyFooter />
     </div>
   );
---
+++ M4D8/netflix-react/src/components/showDetails/ShowDetails.jsx
@@ -0,0 +1,92 @@
+import { Component } from 'react';
+import {
+  Row,
+  Col,
+  Card,
+  ListGroup,
+  ListGroupItem,
+  Container,
+} from 'react-bootstrap';
+
+class ShowDetails extends Component {
+  state = {
+    movieToShow: null,
+    movies: [],
+  };
+
+  componentDidMount = async (props) => {
+    try {
+      const response = await fetch(
+        'http://www.omdbapi.com/?apikey=9dd1231b&s=comedy'
+      );
+      console.log(response);
+      if (response.ok) {
+        let data = await response.json();
+        console.log('HERE', data.Search);
+        console.log(this.props.history);
+        this.setState({
+          movies: data.Search,
+        });
+      } else {
+        console.log('we got an error');
+        this.setState({ isError: true, isLoading: false });
+      }
+    } catch (error) {
+      console.log(error);
+      this.setState({ isError: true, isLoading: false });
+    }
+    let idFromUrl = this.props.match.params.check;
+    console.log('How it looks like', idFromUrl);
+    console.log('MOVIES ARRAY', this.state.movies);
+    let foundedMovie = this.state.movies.find(
+      (movie) => movie.imdbID === idFromUrl
+    );
+    if (foundedMovie) {
+      console.log('FOUNDED MOVIE', foundedMovie);
+      this.setState({
+        movieToShow: foundedMovie,
+      });
+      console.log('CHECK STATE HERE', this.state);
+    }
+  };
+
+  render() {
+    return (
+      <>
+        {this.state.movieToShow && (
+          <div>
+            <h2> {this.state.movieToShow.Title}</h2>
+
+            <Container>
+              <Row>
+                <Col xs={4}>
+                  <Card>
+                    <Card.Img
+                      variant="top"
+                      src={this.state.movieToShow.Poster}
+                    />
+                    <Card.Body>
+                      <Card.Title>n</Card.Title>
+                    </Card.Body>
+                    <ListGroup className="list-group-flush text-dark">
+                      <ListGroupItem>
+                        TYPE: {this.state.movieToShow.Type}
+                      </ListGroupItem>
+                      <ListGroupItem>
+                        ID of Movie: {this.state.movieToShow.imdbID}
+                      </ListGroupItem>
+                    </ListGroup>
+                  </Card>
+                </Col>
+                <Col xs={8}>Place for comments:</Col>
+              </Row>
+            </Container>
+          </div>
+        )}
+      </>
+    );
+  }
+}
+
+export default ShowDetails;
+© 2021 GitHub, Inc.
