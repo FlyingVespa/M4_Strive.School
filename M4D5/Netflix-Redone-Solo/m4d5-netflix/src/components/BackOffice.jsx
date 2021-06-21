2cb1944938486bb5d27131e1575d3679b176f938
Helena Nel <mailhedri@gmail.com>
2021-06-21 11:12:44 +0200

    Update BackOffice.jsx

--- M4D5/Netflix-Redone-Solo/m4d5-netflix/src/components/BackOffice.jsx
+++ M4D5/Netflix-Redone-Solo/m4d5-netflix/src/components/BackOffice.jsx
@@ -2,6 +2,29 @@ import React, { Component } from "react";
 import { Form, Button } from "react-bootstrap";
 
 export default class BackOffice extends Component {
+  state = {
+    comment: {},
+  };
+  componentDidMount = async()=> {
+    const commentURL = `https://striveschool-api.herokuapp.com/api/comments/`;
+    const APIkey = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MGM1MzExZDcwNDBkZjAwMTU4NWM4MDIiLCJpYXQiOjE2MjM2MDYyMjYsImV4cCI6MTYyNDgxNTgyNn0.X_v8ZgYjqf7ggYTB9FEPB6S9dfBb88D4LKJ80Mn425c";
+    try {
+      let res = await fetch(commentURL, { method:"GET",
+        headers: {
+        "Content-Type": "application/json",
+        Authorization:APIkey
+      }},{
+     
+      });
+      let commentData = await res.json();
+      console.log(commentData);
+      this.setState({ comment: commentData });
+    } catch (err) {
+      console.error(err);
+    }
+  };
+  }
+  
   render() {
     return (
       <div>
